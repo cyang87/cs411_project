003bde1bced49698c25caec773751a5e6d2e8153
Jeff Yang <jeffyang@wirelessprv-10-195-146-45.near.illinois.edu>
2019-03-24 15:25:31 -0500

    button calls backend function

--- cs411_app/views.py
+++ cs411_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
 
@@ -6,9 +10,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    print(request)
     return render(request, 'index.html')
     #return HttpResponse("CS 411 Project: Development Environment Setup")
 
 def test(request):
+    return render(request, 'test.html')
+
+@csrf_protect
+def insert_record(request):
+    print("insert_record")
+    print(request.GET["input_test"])
+
+
+
     return render(request, 'test.html')
