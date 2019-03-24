61e6572faa251669d127ea417e7c50c6f30d4d6b
Jeff Yang <jeffyang@wirelessprv-10-195-146-45.near.illinois.edu>
2019-03-24 16:07:03 -0500

    calling sql query from frontend

--- cs411_app/models.py
+++ cs411_app/models.py
@@ -11,3 +11,25 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Easy(models.Model):
+    first = models.IntegerField(default=0)
+
+# +-----------------+--------------+------+-----+---------+-------+
+# | Field           | Type         | Null | Key | Default | Extra |
+# +-----------------+--------------+------+-----+---------+-------+
+# | YEAR            | int(11)      | NO   | PRI | NULL    |       |
+# | C113_CAUSE_NAME | text         | NO   |     | NULL    |       |
+# | CAUSE_NAME      | varchar(100) | NO   | PRI | NULL    |       |
+# | STATE           | varchar(100) | NO   | PRI | NULL    |       |
+# | DEATHS          | int(11)      | YES  |     | NULL    |       |
+# | AADR            | double       | YES  |     | NULL    |       |
+# +-----------------+--------------+------+-----+---------+-------+
+
+# class Cause(models.Model):
+#     YEAR = models.IntegerField(default=0)
+#     C113_CAUSE_NAME = models.CharField(max_length=200)
+#     CAUSE_NAME = models.CharField(max_length=200)
+#     STATE = models.CharField(max_length=200)
+#     DEATH = models.IntegerField(default=0)
+#     AADR = models.DoubleFIeld(default=0)
--- cs411_app/views.py
+++ cs411_app/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import ensure_csrf_cookie
+from cs411_app.models import Easy
 
 # Create your views here.
 
@@ -21,6 +22,7 @@ def insert_record(request):
     print("insert_record")
     print(request.GET["input_test"])
 
-
+    p = Easy(request.GET["input_test"])
+    p.save()
 
     return render(request, 'test.html')
