a78bad1e7349a7f6d2e610b9f1198724af098e8f
Pooja Kankani <kankani2@illinois.edu>
2019-03-25 23:09:19 -0500

    added analyze

--- cs411_app/views.py
+++ cs411_app/views.py
@@ -103,9 +103,14 @@ def search_record(request):
 
 @csrf_exempt
 def updated_page(request):
-    query1 = ""
+    user_id = str(request.GET.get("user_id")) 
+    user_name = str(request.POST["name"])
+    state = str(request.POST["state"])
+    age = int(request.POST["age"])
 
-
+    query1 = "UPDATE user_profile SET user_name = %s, state = %s, age = %s WHERE user_id = %s"
+    cursor = connection.cursor()
+    cursor.execute(query1, [user_name, state, age, user_id])
     query2 = "SELECT * from user_profile"
     cursor = connection.cursor()
     cursor.execute(query2)
@@ -137,9 +142,10 @@ def deleted_page(request):
 
 @csrf_exempt
 def analyze(request):
-    query1 = "SELECT causes1.CAUSE_NAME, causes1.STATE, causes1.deaths FROM causes as causes1, (SELECT STATE, max(Deaths) as max_deaths FROM causes  WHERE STATE = 'California' and C113_CAUSE_NAME!='All Causes' GROUP BY STATE) causes2 WHERE causes1.STATE = causes2.STATE and causes2.max_deaths = causes1.DEATHS;"
+    user_state = str(request.GET.get("state"))
+    query1 = "SELECT causes1.CAUSE_NAME, causes1.STATE, causes1.deaths FROM causes as causes1, (SELECT STATE, max(Deaths) as max_deaths FROM causes  WHERE STATE = %s and C113_CAUSE_NAME!='All Causes' GROUP BY STATE) causes2 WHERE causes1.STATE = causes2.STATE and causes2.max_deaths = causes1.DEATHS;"
     cursor = connection.cursor()
-    cursor.execute(query1)
+    cursor.execute(query1, [user_state])
     result = cursor.fetchall()
     columns = cursor.description
     result = [{columns[index][0]: column for index, column in enumerate(value)} for value in result]
