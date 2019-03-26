2cea792952b3e152427cc87cd5584178fc95d0e4
Jeff Yang <jeffyang@wirelessprv-10-195-146-45.near.illinois.edu>
2019-03-25 20:49:07 -0500

    search username

--- cs411_app/urls.py
+++ cs411_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('cs411_app/', views.index, name='index'),
     path('insert_record/', views.insert_record, name='insert_record'),
+    path('search_record/', views.search_record, name='search_record'),
 ]
